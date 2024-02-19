d9c9afc14c232505fae28009c034301a9284a3a9
thibaut.cizeron <thib.czr2002@gmail.com>
2024-02-19 17:34:54 +0100

    condition restart button

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 // Liste de mots
-const mots = [  "Soleil",
-                "Orange",
-                "Tomate",
-                "Jardin",
-                "Souris",
-                "Livres",
-                "Gâteau",
-                "Manger"
-] 
+const mots = [
+    "SOLEIL",
+    "ORANGE",
+    "TOMATE",
+    "JARDIN",
+    "SOURIS",
+    "LIVRES",
+    "GATEAU",
+    "MANGER"
+];
+
+var motSelectionne;
 
 function startGame() {
-    var motSelectionne;
     var indexAleatoire = Math.floor(Math.random() * mots.length);
     motSelectionne = mots[indexAleatoire];
     console.log(motSelectionne);
@@ -35,21 +37,44 @@ window.addEventListener("load", function() {
     genererGrille();
 });
 
-
-function saisie(){
+function saisie() {
     var inputField = document.getElementById("inputField");
     inputField.addEventListener("keydown", function(event) {
-        if (event.keyCode === 13) {
-            var valeur = event.target.value;
-            console.log("Contenu du champ de saisie :", valeur);
-            event.target.value = "";
+        if (event.key === "Enter") {
+            var val = event.target.value.toUpperCase(); 
+            var regex = /[^A-Z]/;
+            var info = document.getElementById("info"); 
+            switch (true) {
+                case regex.test(val):
+                    info.innerText = "Le mot ne doit contenir que des lettres de l'alphabet";
+                    info.style.color = "red";
+                    event.target.value = "";
+                    break;
+                case val.includes(' '):
+                    info.innerText = "La valeur ne doit pas contenir d'espaces";
+                    info.style.color = "red";
+                    event.target.value = "";
+                    break;
+                case val.length !== 6:
+                    info.innerText = "Le mot doit contenir 6 lettres";
+                    event.target.value = "";
+                    break;
+                case val !== motSelectionne:
+                    info.innerText = "Ce n'est pas la valeur attendue.";
+                    info.style.color = "red";
+                    event.target.value = "";
+                    break;
+                default:
+                    info.innerText = "Bravo, la saisie est correcte !";
+                    info.style.color = "green";
+                }
         }
     });
 }
 saisie();
 
-
-
-
-
+var restart = document.getElementById("restart");
+restart.addEventListener("click",function(){
+    startGame()
+});
 
